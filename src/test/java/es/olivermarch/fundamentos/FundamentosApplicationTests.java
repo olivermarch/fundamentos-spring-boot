5ba723486d90eb21ca3d8ff2a6ecbf3537198336
Oliver March <march89@gmail.com>
2022-06-17 18:54:19 +0100

    first commit

---
+++ src/test/java/es/olivermarch/fundamentos/FundamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package es.olivermarch.fundamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
